1d1c605dcea3b28fc4e59490a53013e41596603c
Hanna.Jang <wkdgkssk0711@gmail.com>
2025-04-13 13:28:16 +0900

    csv_to_json 파일 수정 (content 칼럼 제외)

--- db/insert_data.py
+++ db/insert_data.py
@@ -4,9 +4,11 @@ from config.db_config import get_database
 def insert_data(place_info_json, reviews_json):
     db = get_database()
 
+    #place_info 컬렉션 생성 및 적재
     db["place_info"].drop()
     db["place_info"].insert_many(place_info_json)
 
+    #reviews 컬렉션 생성 및 적재
     db["reviews"].drop()
     db["reviews"].insert_many(reviews_json)
 
--- preprocess/csv_to_json.py
+++ preprocess/csv_to_json.py
@@ -2,8 +2,13 @@ import pandas as pd
 
 #place_info, reviews 데이터 프레임을 json 형식으로 저장
 def convert_to_json(df_place_info: pd.DataFrame, df_reviews: pd.DataFrame):
+    #df_reviews에서 content 칼럼 제외
+    df_reviews = df_reviews[["place_id", "visit_count", "content_nouns"]]
+
+    #df_place_info, reviews json 형식으로 및 저장
     place_info_json = df_place_info.to_dict(orient="records")
     reviews_json = df_reviews.to_dict(orient="records")
+
     return place_info_json, reviews_json
 
 
