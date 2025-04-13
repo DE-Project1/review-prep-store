556455636d2aad31dbf3eaf0ebf4c0a759c0fc91
Kaeun Han <124423333+kkaeunhan@users.noreply.github.com>
2025-04-13 22:24:00 +0900

    fix : MongoDB Atlas 접속 시 SSL 인증서 검증 문제 해결

--- config/db_config.py
+++ config/db_config.py
@@ -1,19 +1,21 @@
 import os
+import certifi
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
 # .env 파일에서 환경 변수 불러오기
 load_dotenv(dotenv_path="./env/.env")
 
-#client 객체 생성
+# client 객체 생성
 def get_mongo_client():
     mongo_uri = os.getenv("MONGO_URI")
-    client = MongoClient(mongo_uri)
+    client = MongoClient(mongo_uri, tlsCAFile=certifi.where())  # 🔐 인증서 경로 지정
     return client
 
-#데이터베이스 가져오기
+# 데이터베이스 가져오기
 def get_database():
     client = get_mongo_client()
     db = client[os.getenv("DB_NAME")]
     return db
+
 
